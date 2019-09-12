98013e94c8f4814dc5770602e774596c81d24c95
imyeyu <imyeyu@qq.com>
2019-09-12 21:20:29 +0800

    优化算法

--- YeyuUtils/src/net/imyeyu/utils/Configer.java
+++ YeyuUtils/src/net/imyeyu/utils/Configer.java
@@ -12,43 +12,25 @@ public class Configer {
 		this.APP_NAME = new Propertier().getValue("appName");
 	}
 
-	public void setConfig(Map<String, Object> map) {
+	public void set(Map<String, Object> map) {
 		map = YeyuUtils.tools().sortMapByKey(map);
-		YeyuUtils.file().stringToFile(new File(APP_NAME + ".ini"), configToString(map));
-	}
-	
-	public String configToString(Map<String, Object> map) {
 		StringBuffer sb = new StringBuffer();
-		int i = 0, size = map.size();
 		for (Map.Entry<String, Object> entry : map.entrySet()) {
-			if (i < size) {
-				sb.append(entry.getKey() + "=" + entry.getValue() + "\r\n");
-			} else {
-				sb.append(entry.getKey() + "=" + entry.getValue());
-			}
+			sb.append(entry.getKey() + "=" + entry.getValue() + "\r\n");
 		}
-		return sb.toString();
+		YeyuUtils.file().stringToFile(new File(APP_NAME + ".ini"), sb.toString());
 	}
 
-	public Map<String, Object> getConfig() {
+	public Map<String, Object> get() {
 		Map<String, Object> map = new HashMap<String, Object>();
 		String data = YeyuUtils.file().fileToString(new File(APP_NAME + ".ini"), "UTF-8");
-		String[] config = data.split("\r\n");
-		String[] keyValue = null;
-
-		for (int i = 0, l = config.length; i < l; i++) {
-			keyValue = config[i].split("=");
-			if (keyValue.length > 1) {
-				map.put(keyValue[0], keyValue[1]);
-			} else {
-				map.put(keyValue[0], "");
-			}
+		String[] configs = data.split("\r\n|[\r\n]");
+		String key, value;
+		for (int i = 0, l = configs.length; i < l; i++) {
+			key = configs[i].substring(0, configs[i].indexOf("="));
+			value = configs[i].substring(configs[i].indexOf("=") + 1, configs[i].length());
+			map.put(key, value);
 		}
-		
 		return map;
-	}
-	
-	public void setDefaultConfig(Map<String, Object> map) {
-		setConfig(YeyuUtils.tools().sortMapByKey(map));
 	}
 }
